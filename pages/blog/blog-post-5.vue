6c126383cf59f0c189ccf199dc4acc0ccf12c9bb
ybebee9 <ybebee9@gmail.com>
2020-03-01 14:01:18 +0900

    블로그 커스터마이징 작업중

--- pages/blog/blog-post-5.vue
+++ pages/blog/blog-post-5.vue
@@ -1,25 +1,25 @@
 <template lang="md">
-# Hello World!  
-  
-Current route is: {{ $route.path }}  
-  
-*Italic*, **bold**, and 'monospace'  
-  
-    ```js  
+# Hello World!
+
+Current route is: {{ $route.path }}
+
+*Italic*, **bold**, and 'monospace'
+
+    ```js
     <script>
         const foo = 'bar';
         console.log(foo);
-    </script
-    ```  
-  
-    Itemized lists look like:  
-  
-        * this one  
-        * that one  
-        * the other one  
-  
-> Block quotes are  
-> written like so.  
+    </script>
+    ```
+
+    Itemized lists look like:
+
+        * this one
+        * that one
+        * the other one
+
+> Block quotes are
+> written like so.
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,31 @@
 <template>
+  <div>
+    <!-- 블로그 Navigation   -->
+    <BlogNavigation></BlogNavigation>
+    <!-- 블로그 header -->
+    <BlogHeader></BlogHeader>
+    <!-- 블로그 메인 content -->
+    <BlogMainContent></BlogMainContent>
+    <hr>
+    <!-- 블로그 footer -->
+    <BlogFooter></BlogFooter>
+  </div>
 
 </template>
 
 <script>
+  import BlogNavigation from "~/components/BlogNavigation.vue"
+  import BlogHeader from "~/components/BlogHeader.vue"
+  import BlogMainContent from "~/components/BlogMainContent.vue"
+  import BlogFooter from "~/components/BlogFooter.vue"
 
 
 export default {
-  computed: {
-
+  components: {
+    'BlogNavigation': BlogNavigation,
+    'BlogHeader': BlogHeader,
+    'BlogMainContent': BlogMainContent,
+    'BlogFooter': BlogFooter
   }
 }
 </script>
